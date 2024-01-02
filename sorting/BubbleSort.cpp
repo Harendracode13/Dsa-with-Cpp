c8a2afcfb06380678aa4a54ce1e70497ab820cd4
Harendra Piprotar <piprotarharendra@gmail.com>
2024-01-02 14:49:20 +0530

    Bubble sort
    This bubble sort. using for sorting a array

---
+++ sorting/BubbleSort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - i - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    const int size = 5;
+    int arr[size] = {64, 25, 12, 22, 11};
+
+    cout << "Given array: ";
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+    }
+
+    // Call the bubbleSort function
+    bubbleSort(arr, size);
+
+    cout << "\nSorted array: ";
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
