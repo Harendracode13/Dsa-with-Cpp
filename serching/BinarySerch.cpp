083ff8b47782b5baf6a93fe0f9cb0f4802858136
Harendra Piprotar <146612793+Harendracode13@users.noreply.github.com>
2024-01-05 21:55:22 +0530

    Update BinarySerch

--- serching/BinarySerch.cpp
+++ serching/BinarySerch.cpp
@@ -27,13 +27,9 @@ int BinarySerch(int arr[],int size,int key)
 
 int main()
 {
-<<<<<<< HEAD
    int arr[6]={5,6,23,30,34,45};
    int arr1[5]={4,7,34,44,46};
-=======
-   int arr[6]={5,6,23,30,34,40};
-   int arr1[5]={3,5,6,50,54};
->>>>>>> 019ddbc0512284f226db9f59fa2d4ca8e59f85f5
+
 
    int index=BinarySerch(arr,6,30);
    cout<<"the key is present at index :"<<index<<"\n";
