222c09914858cbfd5ffd62d47d4458f44b610614
Harendra Piprotar <piprotarharendra@gmail.com>
2024-01-18 22:14:52 +0530

    Tree Travels

---
+++ Tree/Travels.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+
+using namespace std;
+
+class Node
+{
+
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+};
+
+Node *buildtree(Node *root)
+{
+    cout << "Enter the data: ";
+    int data;
+    cin >> data;
+    cout << "\n";
+    root = new Node(data);
+
+    if (data == -1)
+    {
+        return nullptr;
+    }
+
+    cout << "Enter data for inserting in left " << data << ":\nif you don't Want enter in left side then enter -1: \n";
+    root->left = buildtree(root->left);
+    cout << "Enter data for inserting in right " << data << ":\nif you don't Want enter in left side then enter -1: \n";
+    root->right = buildtree(root->right);
+
+    return root;
+}
+// inorder travels
+void inordertravels(Node *root)
+{
+    if (root == nullptr)
+    {
+        return;
+    }
+    inordertravels(root->left);
+    cout << root->data << " ";
+    inordertravels(root->right);
+}
+
+//preorder travels
+void preordertravels(Node *root)
+{
+    if (root == nullptr)
+    {
+        return;
+    }
+    cout << root->data << " ";
+    inordertravels(root->left);
+    inordertravels(root->right);
+}
+
+//post order travels
+void postordertravels(Node *root)
+{
+    if (root == nullptr)
+    {
+        return;
+    }
+    inordertravels(root->right);
+    inordertravels(root->left);
+    cout << root->data << " ";
+}
+int main()
+{
+    Node *root = nullptr;
+
+    root = buildtree(root);
+    cout << "preorder travels : ";
+    preordertravels(root);
+    cout << "\n";
+    cout << "Inorder travels : ";
+    inordertravels(root);
+    cout << "\n";
+    cout << "postorder travels : ";
+    postordertravels(root);
+    cout << "\n";
+
+    return 0;
+}
