6df3ebd738a6db8f9c1fd7183670d1d1fb55efb6
Harendra Piprotar <piprotarharendra@gmail.com>
2024-04-05 21:47:01 +0530

    Dynamically create 2d array

---
+++ Pointer/2D_array.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int row;
+    cin >> row;
+
+    int col;
+    cin >> col;
+
+    //creating a 2D array in heap (dynamically)
+    int** arr = new int*[row];
+    for(int i=0; i<row; i++) {
+        arr[i] = new int[col];
+    }
+
+    //taking input
+    for(int i=0; i<row; i++) {
+        for(int j=0; j<col; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    // output
+    cout << endl;
+    for(int i=0; i<row; i++) {
+        for(int j=0; j<col; j++) {
+            cout << arr[i][j] << " ";
+        } cout << endl;
+    }
+
+
+    //releasing memory by manuly
+    for(int i=0; i<row; i++) {
+        delete [] arr[i];
+    }   
+
+    delete []arr;
+    return 0;
+}
