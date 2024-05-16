3ff207259928a592d82201d60fe40df1eec89078
Harendra Piprotar <piprotarharendra@gmail.com>
2024-05-16 16:50:52 +0530

    Print String

---
+++ problem-solution/string.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    string s;
+    cout<<"enter the string you want\n";
+    cin>>s;
+    cout<<"your string is "<<s;
+    return 0;
+}  
