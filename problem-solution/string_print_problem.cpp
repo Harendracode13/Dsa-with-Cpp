9ff9b1e01c3a0efbf6bed3b406ed6ecf9de71e79
Harendra Piprotar <piprotarharendra@gmail.com>
2024-05-16 16:49:38 +0530

    Take input in string

--- problem-solution/string_print_problem.cpp
+++ problem-solution/string_print_problem.cpp
@@ -2,9 +2,15 @@
 
 #include <iostream>
 #include <sstream>
-#include<string>
-
+#include<string>	
+#include<bits/stdc++.h>
+ 
 using namespace std;
+ 
+//macro definitions
+#define MAX_NAME_LEN 60  // Maximum len of your name can't be more than 60
+#define MAX_ADDRESS_LEN 120  // Maximum len of your address can't be more than 120
+#define MAX_ABOUT_LEN 250 // Maximum len of your profession can't be more than 250
 
 int main()
 {
@@ -19,6 +25,23 @@ int main()
 	while (obj >> temp) {
 		cout << temp << endl;
 	}
+
+  char y_name[MAX_NAME_LEN], y_address[MAX_ADDRESS_LEN], about_y[MAX_ABOUT_LEN];
+ 
+  cout << "Enter your name: ";
+  cin.getline (y_name, MAX_NAME_LEN);
+ 
+  cout << "Enter your City: ";
+  cin.getline (y_address, MAX_ADDRESS_LEN);
+ 
+  cout << "Enter your profession (press $ to complete): ";
+  cin.getline (about_y, MAX_ABOUT_LEN, '$');    //$ is a delimiter 
+ 
+  cout << "\nEntered details are:\n"<<'\n';
+  cout << "Name: " << y_name << endl;
+  cout << "Address: " << y_address << endl;
+  cout << "Profession is: " << about_y << endl;
+
 	return 0;
     
 }
